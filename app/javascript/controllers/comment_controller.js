c366c8667dd5e901af127b5810435b877e006511
soadaa <Soadhlh@gmail.com>
2023-09-27 12:29:10 -0400

    Edit btn text changes on click
    A slight problem I'm having is the need to run yarn build to update my javascript changes. This doesn't occur with HTML or CSS changes. I should make a script to run yarn build then start server in development or look into the javascript issue further.

--- app/javascript/controllers/comment_controller.js
+++ app/javascript/controllers/comment_controller.js
@@ -2,13 +2,24 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="comment"
 export default class extends Controller {
-  initialize() {
-    console.log("Initialized!");
-  }
-  connect() {
-    console.log("Testing 123");
-  }
-  toggleForm() {
+  initialize() {  }
+  connect() {  }
+  toggleForm(e) {
     console.log("I clicked the edit button")
+    e.preventDefault();
+    e.stopPropagation();
+    const formID = e.params["form"];
+    const commentBodyID = e.params["body"];
+    const editButtonID = e.params["button"];
+    const form = document.getElementById(formID)
+    const commentBody = document.getElementById(commentBodyID);
+    const editButton = document.getElementById(editButtonID);
+    form.classList.toggle("d-none");
+    commentBody.classList.toggle("d-none");
+    if (editButton.innerHTML == "Back") {
+      editButton.innerHTML = "Edit"
+    } else {
+      editButton.innerHTML = "Back"
+    }
   }
 }
