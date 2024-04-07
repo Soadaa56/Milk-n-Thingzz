d07150f204761e9fbacbfde6773ad0c571886faa
soadaa <Soadhlh@gmail.com>
2024-04-07 16:51:29 -0400

    Descriptions for crafts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_23_235402) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_07_201828) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -73,6 +73,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_23_235402) do
     t.datetime "updated_at", null: false
     t.integer "comments_count"
     t.text "image_data"
+    t.text "description"
   end
 
   create_table "users", force: :cascade do |t|
