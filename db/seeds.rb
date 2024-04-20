d3f0d82ea6459ef85053f3405a1e4284e392f44e
soadaa <Soadhlh@gmail.com>
2024-04-20 15:07:55 -0400

    Uncomment seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Initial Craft table seed
 
-# Initial Craft table seed. Saved incase of data lost.
-
-# Craft.create(name: 'Flower Coffin', category: '3D-Print')
-# Craft.create(name: 'Skull Pot', category: '3D-Print')
-# Craft.create(name: 'Mushroom Earrings', category: '3D-Print')
-# Craft.create(name: 'Vase, White', category: '3D-Print')
-# Craft.create(name: 'Flower Earrings, Pink', category: '3D-Print')
-# Craft.create(name: 'Scarf, Blue and Gray', category: 'Crotchet')
-# Craft.create(name: 'Scarf, Gray, Yellow, Pink and Purple', category: 'Crotchet')
-# Craft.create(name: 'Scarf, Pastel Rainbow', category: 'Crotchet')
-# Craft.create(name: 'Scarf, Blue', category: 'Crotchet')
-# Craft.create(name: 'Ice King Crown', category: 'Embroidery')
-# Craft.create(name: 'Companion Cube', category: 'Embroidery')
+Craft.create(name: 'Flower Coffin', category: '3D-Print')
+Craft.create(name: 'Skull Pot', category: '3D-Print')
+Craft.create(name: 'Mushroom Earrings', category: '3D-Print')
+Craft.create(name: 'Vase, White', category: '3D-Print')
+Craft.create(name: 'Flower Earrings, Pink', category: '3D-Print')
+Craft.create(name: 'Scarf, Blue and Gray', category: 'Crotchet')
+Craft.create(name: 'Scarf, Gray, Yellow, Pink and Purple', category: 'Crotchet')
+Craft.create(name: 'Scarf, Pastel Rainbow', category: 'Crotchet')
+Craft.create(name: 'Scarf, Blue', category: 'Crotchet')
+Craft.create(name: 'Ice King Crown', category: 'Embroidery')
+Craft.create(name: 'Companion Cube', category: 'Embroidery')
