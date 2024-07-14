757c38d3a63d709cb4546e5d4f2fdb1616aae874
soadaa <Soadhlh@gmail.com>
2024-07-14 18:30:15 -0400

    Menu button toggle; bugged

--- app/javascript/controllers/menu_controller.js
+++ app/javascript/controllers/menu_controller.js
@@ -1,1 +1,24 @@
 import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  initialize() {  }
+  connect() {
+    console.log("test connection");
+    const menuButton = document.getElementById("mobile-menu-button");
+    if (menuButton) {
+      menuButton.addEventListener("click", this.toggleMenu.bind(this));
+    }
+  }
+
+  toggleMenu(e){
+    e.preventDefault();
+    e.stopPropagation();
+
+    const menuMobile = document.getElementById("mobile-popup-menu");
+    menuMobile.classList.toggle("d-none");
+    console.log("test button")
+  }
+
+
+
+}
