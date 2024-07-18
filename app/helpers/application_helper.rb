1fbb8de6d48c249b1490c317bd05e76b8f777f5a
soadaa <Soadhlh@gmail.com>
2024-07-18 15:15:29 -0400

    Craft show pages link to category pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def home_craft_category_path(craft)
+    case craft.category.downcase
+    when 'print'
+      home_print_path
+    when 'crotchet'
+      home_crotchet_path
+    when 'embroidery'
+      home_embroidery_path
+    when 'painting'
+      home_painting_path
+    when 'miscellaneous'
+      home_miscellaneous_path
+    else
+      '#'
+    end
+  end
 end
