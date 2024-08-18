e7a9d9341ab107f12e88d1b30234d278d2ccd505
soadaa <Soadhlh@gmail.com>
2024-08-18 19:53:10 -0400

    Remove unneeded console log messages from controller

--- app/javascript/controllers/subtype_sort_controller.js
+++ app/javascript/controllers/subtype_sort_controller.js
@@ -14,10 +14,6 @@ export default class extends Controller {
     const sortBy = this.selectTarget.value;
     const url = `/home/earring?sort_by=${sortBy}`;
 
-    console.log("element", this.element.value);
-    console.log("sortby", sortBy);
-    console.log("url", url);
-
     Turbo.visit(url);
   }
 }
