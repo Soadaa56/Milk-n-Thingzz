5ad1f98b9d0ab9b428a87730b2a6cd6a0e66b18b
soadaa <Soadhlh@gmail.com>
2024-08-21 17:26:22 -0400

    Variable for navbar to have unique subtypes to list

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_subtypes
 
   protected
 
@@ -8,5 +9,11 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: [:login, :password]
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
+
+  private
+
+  def set_subtypes
+    @earring_subtypes = Craft.where(category: "Earring").pluck(:subtype).uniq
   end
 end
