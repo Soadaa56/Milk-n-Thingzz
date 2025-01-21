04abeb1c67a2a64618a484ca48c73aaa9ab39434
soadaa <Soadhlh@gmail.com>
2025-01-21 13:29:59 -0500

    Fix broken category links on craft show pages

--- app/helpers/crafts_helper.rb
+++ app/helpers/crafts_helper.rb
@@ -1,2 +1,18 @@
 module CraftsHelper
+  def craft_category_path_helper(craft)
+    case craft.category
+    when 'Crotchet'
+      home_crotchet_path
+    when 'Embroidery'
+      home_embroidery_path
+    when '3D-Print'
+      home_print_path
+    when 'Earring'
+      home_earring_path
+    when 'Painting'
+      home_painting_path
+    else
+      home_craft_category_path(craft)
+    end
+  end
 end
