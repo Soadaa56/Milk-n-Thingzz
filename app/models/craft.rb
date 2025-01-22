f5abe978cad0f6c6ac682608dd22aad3e653d169
soadaa <Soadhlh@gmail.com>
2025-01-22 14:46:09 -0500

    Crafts validate having at least one image

--- app/models/craft.rb
+++ app/models/craft.rb
@@ -5,6 +5,13 @@ class Craft < ApplicationRecord
   include ImageUploader::Attachment(:image)
   has_many :craft_images, dependent: :destroy
   accepts_nested_attributes_for :craft_images, allow_destroy: true
+  validate :must_have_at_least_one_image
+
+  def must_have_at_least_one_image
+    if craft_images.empty?
+      errors.add(:base, "Craft must have at least one image")
+    end
+  end
 end
 
 
