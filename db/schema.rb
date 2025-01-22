5f07edc6119e8fff78d9bb1911c651a6fbe6b820
soadaa <Soadhlh@gmail.com>
2025-01-22 14:59:08 -0500

    CraftImage posistions to allow image reordering
    Should also allow me to set one image as the main for sorted page display.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_05_024910) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_22_195743) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -63,6 +63,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_05_024910) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "image_data"
+    t.integer "position"
     t.index ["craft_id"], name: "index_craft_images_on_craft_id"
   end
 
