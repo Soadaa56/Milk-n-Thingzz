fc0f2046e3b5b15c5d80c3cc283c3ddb9ead1dfe
Kevin Duffy <soadhlh@gmail.com>
2025-04-16 16:36:44 -0400

    Specify file to precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,7 +9,7 @@ Rails.application.config.assets.version = "1.0"
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( application.bootstrap.scss )
+Rails.application.config.assets.precompile += %w( 'app/assets/builds/application.css' )
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
