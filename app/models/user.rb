82f478fa94d5af48526b95646a7b4fe3643dfa62
Kevin Duffy <soadhlh@gmail.com>
2026-07-18 23:54:02 -0400

    Disjoint user comments from user for destory purposes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   enum role: { standard:0, admin: 1 }
-  has_many :comments, dependent: :destroy
+  has_many :comments
   before_validation :set_default_role
 
   attr_writer :login
