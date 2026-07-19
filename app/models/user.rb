fe6aac57e55e867197ebc67ef92f19e09af49186
Kevin Duffy <soadhlh@gmail.com>
2026-07-19 00:04:26 -0400

    Minor: Undo previous

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   enum role: { standard:0, admin: 1 }
-  has_many :comments
+  has_many :comments, dependent: :destroy
   before_validation :set_default_role
 
   attr_writer :login
