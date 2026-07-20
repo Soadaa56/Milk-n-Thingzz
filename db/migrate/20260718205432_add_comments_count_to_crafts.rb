1159d2fbb3de2efe4819957e3a5dafcf857231aa
Kevin Duffy <soadhlh@gmail.com>
2026-07-20 00:55:12 -0400

    Remove column counter from craft table

--- db/migrate/20260718205432_add_comments_count_to_crafts.rb
+++
@@ -1,5 +0,0 @@
-class AddCommentsCountToCrafts < ActiveRecord::Migration[7.2]
-  def change
-    add_column :crafts, :comments_count, :integer
-  end
-end
--- db/migrate/20260718210913_populate_craft_comments_count.rb
+++
@@ -1,7 +0,0 @@
-class PopulateCraftCommentsCount < ActiveRecord::Migration[7.2]
-  def up
-    Craft.find_each do |craft|
-      Craft.reset_counters(craft.id, :comments)
-    end
-  end
-end
