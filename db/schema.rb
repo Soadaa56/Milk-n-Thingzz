964fe447f561d8a31bbb7927ff303da30fe3c8ee
Kevin Duffy <soadhlh@gmail.com>
2026-07-20 00:58:21 -0400

    Drop comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_03_22_215949) do
+ActiveRecord::Schema[7.2].define(version: 2026_07_20_045642) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -47,15 +47,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_03_22_215949) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
-
-  create_table "comments", force: :cascade do |t|
-    t.integer "craft_id", null: false
-    t.integer "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["craft_id"], name: "index_comments_on_craft_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "craft_images", force: :cascade do |t|
@@ -95,7 +86,5 @@ ActiveRecord::Schema[7.2].define(version: 2025_03_22_215949) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "comments", "crafts"
-  add_foreign_key "comments", "users"
   add_foreign_key "craft_images", "crafts"
 end
