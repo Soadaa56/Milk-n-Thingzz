adb8cff6c7b19adc9c8d1f05bf388cde4c79e65e
Kevin Duffy <soadhlh@gmail.com>
2026-07-26 17:15:54 -0400

    Minor: allow user signup

--- db/migrate/20260720180353_add_selling_information_to_crafts.rb
+++ db/migrate/20260720180353_add_selling_information_to_crafts.rb
@@ -3,7 +3,7 @@ class AddSellingInformationToCrafts < ActiveRecord::Migration[7.2]
     add_column :crafts, :for_sale, :boolean, default: false, null: false
     add_column :crafts, :has_variants, :boolean, default: false, null: false
     add_column :crafts, :inventory, :integer
-    add_column :crafts, :price, :decimal, precision: 5, scale: 2
+    add_column :crafts, :price, :decimal, precision: 6, scale: 2
     add_column :crafts, :dimensions, :string
   end
 end
