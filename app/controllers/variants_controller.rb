f05a58986c88d55af764dcee550e68cc8c9a6ccf
Kevin Duffy <soadhlh@gmail.com>
2026-07-28 12:19:20 -0400

    bug: params permit shopify id now

--- app/controllers/variants_controller.rb
+++ app/controllers/variants_controller.rb
@@ -66,7 +66,8 @@ class VariantsController < ApplicationController
 
   def variant_params
     params.require(:variant).permit(
-      :name, :sku, :price, :dimensions, :inventory_count, :active,
+      :name, :sku, :price, :dimensions, :inventory_count, 
+      :active, :shopify_variant_id,
       images_attributes: [
         :id, :variant_id, :image, :image_data, :_destroy
       ]
