8a6fb91009a6fc6bfed71d0c13b10c664118215c
Kevin Duffy <soadhlh@gmail.com>
2026-07-28 23:37:41 -0400

    Fix redirect path after sign in/up

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -6,7 +6,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # GET /resource/sign_up
   def new
-    redirect_to root_page
+    redirect_to root_path
   end
 
   # POST /resource
