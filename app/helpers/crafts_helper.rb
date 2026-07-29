637e7de10f6ec813155d3958a48b9a12b806c162
Kevin Duffy <soadhlh@gmail.com>
2026-07-28 22:23:25 -0400

    Minor: easier reading with spaces

--- app/helpers/crafts_helper.rb
+++ app/helpers/crafts_helper.rb
@@ -3,18 +3,25 @@ module CraftsHelper
     case craft.category.downcase
     when 'crochet'
       home_crochet_path
+
     when 'earring'
       home_earring_path
+
     when 'embroidery'
       home_embroidery_path
+
     when '3d-print', 'print'
       home_print_path
+
     when 'miscellaneous', 'misc', 'misc.'
       home_miscellaneous_path
+
     when 'necklace'
       home_necklace_path
+
     when 'painting'
       home_painting_path
+      
     else
       '#'
     end
