a8b4a00bafff2467ad67f6f8333d85d26e41201e
Kevin Duffy <soadhlh@gmail.com>
2026-07-29 00:23:48 -0400

    Remove user create from admin
    Can test in console if needed

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -5,20 +5,12 @@ class AdminController < ApplicationController
   def index
     @users = User.all
   end
-  
-  def create
-    @user = User.new(user_params)
-  end
 
   def update; end
 
   def console; end
 
   private
-
-  def user_params
-    params.permit(:email, :password, :password_confirmation, :role)
-  end
 
   def authorize_admin!
     redirect_to root_path, alert: 'You are not authorized to access this page.' unless current_user.admin?
