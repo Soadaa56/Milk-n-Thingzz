1ce6def3fd132efdf120d292b82028f4deaaf80e
Kevin Duffy <soadhlh@gmail.com>
2026-07-31 15:52:56 -0400

    Allow blank subtype

--- app/javascript/controllers/subtype_controller.js
+++ app/javascript/controllers/subtype_controller.js
@@ -11,7 +11,7 @@ export default class extends Controller {
   updateSubtype(event) {
     const category = event ? event.target.value : this.element.querySelector('select').value;
     const allSubtypes = {
-      "Earring": ["Flower", "Gummy Bear", "Jelly Fish", "Fruit", "Ice Cream", "Dairy", "Sushi" ]
+      "Earring": ["", "Flower", "Gummy Bear", "Jelly Fish", "Fruit", "Ice Cream", "Dairy", "Sushi" ]
     };
     const subtypeSelect = document.querySelector("#craft_subtype");
     const subtypes = allSubtypes[category] || [];
