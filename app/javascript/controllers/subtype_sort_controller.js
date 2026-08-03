88b9c8860d7e4618ed6e3d2ea6b2e9ef6f58f071
Kevin Duffy <soadhlh@gmail.com>
2026-08-03 16:22:02 -0400

    Minor: line spacing

--- app/javascript/controllers/subtype_sort_controller.js
+++ app/javascript/controllers/subtype_sort_controller.js
@@ -3,6 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="subtype-sort"
 export default class extends Controller {
   static targets = ["select"];
+  
   connect() {
   }
 
