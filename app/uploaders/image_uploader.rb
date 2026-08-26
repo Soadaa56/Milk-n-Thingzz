35226ebbc60e3e255ddf7e5d17c1578d1ac452e6
Kevin Duffy <soadhlh@gmail.com>
2026-08-25 20:14:57 -0400

    Slightly smaller image variants
    Render size and quality reduced slightly

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,17 +2,17 @@ class ImageUploader < Shrine
   Attacher.derivatives do |original|
     magick = ImageProcessing::MiniMagick.source(original)
     {
-      small:   magick.resize_to_fill(200, 200)
+      small:   magick.resize_to_fill(180, 180)
                       .convert("webp")
-                      .saver(quality: 65)
+                      .saver(quality: 63)
                       .call,
-      card:         magick.resize_to_fill(600,600)
+      card:         magick.resize_to_fill(500,500)
                       .convert("webp")
-                      .saver(quality: 80)
+                      .saver(quality: 78)
                       .call,
       detail:       magick.resize_to_limit(1200, 1200)
                       .convert("webp")
-                      .saver(quality: 85)
+                      .saver(quality: 83)
                       .call
     }
   end
