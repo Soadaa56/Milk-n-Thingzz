c3587e388bc0f9707a893ab36b37e272cdeaf6c5
Kevin Duffy <soadhlh@gmail.com>
2026-09-22 14:47:22 -0400

    Change nav overlay to match store filters

--- app/javascript/controllers/nav_controller.js
+++ app/javascript/controllers/nav_controller.js
@@ -2,8 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="nav"
 export default class extends Controller {
-  connect() {}
-
   openNav() {
     document.body.classList.add('nav-is-open')
   }
